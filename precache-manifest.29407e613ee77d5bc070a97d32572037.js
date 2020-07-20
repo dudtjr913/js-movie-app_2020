0c87baaddf2821680d532f116913b9b10a6cfddf
Cheon YeongSeok <dudtjr913@naver.com>
2020-07-20 21:58:06 +0900

    Updates

---
+++ precache-manifest.29407e613ee77d5bc070a97d32572037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84f297fb467782e6be3f691ca039900",
+    "url": "/js-movie-app_2020/index.html"
+  },
+  {
+    "revision": "0d60e8fc71e009b4a384",
+    "url": "/js-movie-app_2020/static/css/main.c32f44f6.chunk.css"
+  },
+  {
+    "revision": "f41281baaf6bed10d88c",
+    "url": "/js-movie-app_2020/static/js/2.71590914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-movie-app_2020/static/js/2.71590914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d60e8fc71e009b4a384",
+    "url": "/js-movie-app_2020/static/js/main.8eb640be.chunk.js"
+  },
+  {
+    "revision": "49c25d5ea3e55d106af1",
+    "url": "/js-movie-app_2020/static/js/runtime-main.b9abab8e.js"
+  }
+]);

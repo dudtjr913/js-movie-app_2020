b19212bda73ed44e5811e33ba41439086d927fac
Cheon YeongSeok <dudtjr913@naver.com>
2020-07-21 23:21:12 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rcws-development/js-movie-app_2020//inside/the/app/precache-manifest.5d07cee3bf06a8cca06472bd6cac4728.js"
+  "/js-movie-app_2020/precache-manifest.7871859fbf9aa675005b95e63a38c201.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rcws-development/js-movie-app_2020//inside/the/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-movie-app_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
